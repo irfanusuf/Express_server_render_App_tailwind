b23a3df0ee3280684281c8a4ceb0416680dcc677
Irfan Yousuf <irfanusuf33@gmail.com>
2023-04-24 03:14:09 +0530

    routesDeleted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const router = require("./Routes");
+
 
 const app = express();
 const port = 3000
@@ -9,10 +9,26 @@ const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use("/api/v1", router);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post('/register', registerUser = (req, res) => {
+  const userName = req.body.name;
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  res.json({sucess: true,});
+});
+
+
+app.get("/user", user = (req, res) => {
+  res.send("no users found as there is no data base connected");
+});
+
+
+app.delete("/user", user = (req, res) => {
+  res.send("when there are no users what will u deleted");
 });
 
 app.listen(port, () => {
